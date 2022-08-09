903a78cad9b3af71543361f7678ae03801f775a7
arjun-ingole <arjuningole38@gmail.com>
2022-08-09 19:11:44 +0530

    services: Remove redundant class
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,14 +1,1 @@
-// import 'package:audioplayers/audioplayers.dart';
-//
-// class StreamAudio {
-//   bool isPlaying = false;
-//
-//   void getAudio(Source source) async {
-//     final player = AudioPlayer();
-//     if (isPlaying) {
-//       player.play(source);
-//     } else {
-//       player.pause();
-//     }
-//   }
-// }
+
