824f9f3d82970db519f091eeedbe35ec35fb6878
arjun-ingole <arjuningole38@gmail.com>
2022-08-18 21:45:27 +0530

    Services: Write framework for connecting with WS
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,2 +1,40 @@
+import 'dart:convert';
+import 'package:uranus/widgets/switchBar.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+
+late int heartbeat;
+
+String getSocketURL() {
+  String url = 'wss://listen.moe/gateway_v2';
+  if (selectedSource == MusicSource.JPOP) {
+    url = 'wss://listen.moe/gateway_v2';
+  } else if (selectedSource == MusicSource.KPOP) {
+    url = 'wss://listen.moe/kpop/gateway_v2';
+  }
+  return url;
+}
+
+connect() {
+  WebSocketChannel channel = IOWebSocketChannel.connect(getSocketURL());
+  Stream stream = channel.stream.asBroadcastStream();
+  stream.listen((event) {
+    var data = jsonDecode(event);
+    if (data['op'] == 0) {
+      heartbeat = data['d']['heartbeat'];
+    } else {
+      heartbeat = 45000;
+    }
+    sendPings(channel, heartbeat).then((value) => {});
+  });
+}
+
+Future<void> sendPings(WebSocketChannel channel, int heartbeat) async {
+  await Future.delayed(Duration(milliseconds: heartbeat), () {
+    try {
+      channel.sink.add(jsonEncode({"op": 9}));
+    } catch (e) {
+      connect();
+    }
+  });
+}
