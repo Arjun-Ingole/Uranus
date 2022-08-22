e098390b3fa91bd4a94b349bc0613135a226c5ee
arjun-ingole <arjuningole38@gmail.com>
2022-08-23 02:10:06 +0530

    Revert "SocketData: Regenerate data model"
    This reverts commit 7e9ab9a8b092301beca7439b091a1aea2f76eab3.

--- lib/models/SocketData.dart
+++ lib/models/SocketData.dart
@@ -5,206 +5,130 @@ String musicDataToJson(MusicData data) => json.encode(data.toJson());
 
 class MusicData {
   MusicData({
-    required this.op,
-    required this.d,
-    required this.t,
+    this.op,
+    this.d,
+    this.t,
   });
-  late final int op;
-  late final D d;
-  late final String t;
 
-  MusicData.fromJson(Map<String, dynamic> json) {
-    op = json['op'];
-    d = D.fromJson(json['d']);
-    t = json['t'];
-  }
+  int? op;
+  D? d;
+  String? t;
 
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['op'] = op;
-    _data['d'] = d.toJson();
-    _data['t'] = t;
-    return _data;
-  }
+  factory MusicData.fromJson(Map<String, dynamic> json) => MusicData(
+        op: json["op"],
+        d: D.fromJson(json["d"]),
+        t: json["t"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "op": op,
+        "d": d?.toJson(),
+        "t": t,
+      };
 }
 
 class D {
   D({
-    required this.song,
+    this.song,
     this.requester,
     this.event,
-    required this.startTime,
-    required this.lastPlayed,
-    required this.listeners,
+    this.startTime,
+    this.lastPlayed,
+    this.listeners,
   });
-  late final Song song;
-  late final Null requester;
-  late final Null event;
-  late final String startTime;
-  late final List<LastPlayed> lastPlayed;
-  late final int listeners;
 
-  D.fromJson(Map<String, dynamic> json) {
-    song = Song.fromJson(json['song']);
-    requester = null;
-    event = null;
-    startTime = json['startTime'];
-    lastPlayed = List.from(json['lastPlayed'])
-        .map((e) => LastPlayed.fromJson(e))
-        .toList();
-    listeners = json['listeners'];
-  }
+  Song? song;
+  dynamic? requester;
+  dynamic? event;
+  DateTime? startTime;
+  List<Song>? lastPlayed;
+  int? listeners;
 
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['song'] = song.toJson();
-    _data['requester'] = requester;
-    _data['event'] = event;
-    _data['startTime'] = startTime;
-    _data['lastPlayed'] = lastPlayed.map((e) => e.toJson()).toList();
-    _data['listeners'] = listeners;
-    return _data;
-  }
+  factory D.fromJson(Map<String, dynamic> json) => D(
+        song: Song.fromJson(json["song"]),
+        requester: json["requester"],
+        event: json["event"],
+        startTime: DateTime.parse(json["startTime"]),
+        lastPlayed:
+            List<Song>.from(json["lastPlayed"].map((x) => Song.fromJson(x))),
+        listeners: json["listeners"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "song": song?.toJson(),
+        "requester": requester,
+        "event": event,
+        "startTime": startTime?.toIso8601String(),
+        "lastPlayed": List<dynamic>.from(lastPlayed!.map((x) => x.toJson())),
+        "listeners": listeners,
+      };
 }
 
 class Song {
   Song({
-    required this.id,
-    required this.title,
-    required this.sources,
-    required this.artists,
-    required this.albums,
-    required this.duration,
-    required this.favorite,
+    this.id,
+    this.title,
+    this.sources,
+    this.artists,
+    this.albums,
+    this.duration,
+    this.favorite,
   });
-  late final int id;
-  late final String title;
-  late final List<dynamic> sources;
-  late final List<Artists> artists;
-  late final List<dynamic> albums;
-  late final int duration;
-  late final bool favorite;
 
-  Song.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    sources = List.castFrom<dynamic, dynamic>(json['sources']);
-    artists =
-        List.from(json['artists']).map((e) => Artists.fromJson(e)).toList();
-    albums = List.castFrom<dynamic, dynamic>(json['albums']);
-    duration = json['duration'];
-    favorite = json['favorite'];
-  }
+  int? id;
+  String? title;
+  List<dynamic>? sources;
+  List<Album>? artists;
+  List<Album>? albums;
+  int? duration;
+  bool? favorite;
 
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['id'] = id;
-    _data['title'] = title;
-    _data['sources'] = sources;
-    _data['artists'] = artists.map((e) => e.toJson()).toList();
-    _data['albums'] = albums;
-    _data['duration'] = duration;
-    _data['favorite'] = favorite;
-    return _data;
-  }
+  factory Song.fromJson(Map<String, dynamic> json) => Song(
+        id: json["id"],
+        title: json["title"],
+        sources: List<dynamic>.from(json["sources"].map((x) => x)),
+        artists:
+            List<Album>.from(json["artists"].map((x) => Album.fromJson(x))),
+        albums: List<Album>.from(json["albums"].map((x) => Album.fromJson(x))),
+        duration: json["duration"],
+        favorite: json["favorite"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "sources": List<dynamic>.from(sources!.map((x) => x)),
+        "artists": List<dynamic>.from(artists!.map((x) => x.toJson())),
+        "albums": List<dynamic>.from(albums!.map((x) => x.toJson())),
+        "duration": duration,
+        "favorite": favorite,
+      };
 }
 
-class Artists {
-  Artists({
-    required this.id,
-    required this.name,
-    this.nameRomaji,
-    required this.image,
-  });
-  late final int id;
-  late final String name;
-  late final Null nameRomaji;
-  late final String image;
-
-  Artists.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    nameRomaji = null;
-    image = json['image'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['id'] = id;
-    _data['name'] = name;
-    _data['nameRomaji'] = nameRomaji;
-    _data['image'] = image;
-    return _data;
-  }
-}
-
-class LastPlayed {
-  LastPlayed({
-    required this.id,
-    required this.title,
-    required this.sources,
-    required this.artists,
-    required this.albums,
-    required this.duration,
-    required this.favorite,
-  });
-  late final int id;
-  late final String title;
-  late final List<dynamic> sources;
-  late final List<Artists> artists;
-  late final List<Albums> albums;
-  late final int duration;
-  late final bool favorite;
-
-  LastPlayed.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    sources = List.castFrom<dynamic, dynamic>(json['sources']);
-    artists =
-        List.from(json['artists']).map((e) => Artists.fromJson(e)).toList();
-    albums = List.from(json['albums']).map((e) => Albums.fromJson(e)).toList();
-    duration = json['duration'];
-    favorite = json['favorite'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['id'] = id;
-    _data['title'] = title;
-    _data['sources'] = sources;
-    _data['artists'] = artists.map((e) => e.toJson()).toList();
-    _data['albums'] = albums.map((e) => e.toJson()).toList();
-    _data['duration'] = duration;
-    _data['favorite'] = favorite;
-    return _data;
-  }
-}
-
-class Albums {
-  Albums({
-    required this.id,
-    required this.name,
+class Album {
+  Album({
+    this.id,
+    this.name,
     this.nameRomaji,
     this.image,
   });
-  late final int id;
-  late final String name;
-  late final Null nameRomaji;
-  late final Null image;
 
-  Albums.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    nameRomaji = null;
-    image = null;
-  }
+  int? id;
+  String? name;
+  String? nameRomaji;
+  String? image;
 
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['id'] = id;
-    _data['name'] = name;
-    _data['nameRomaji'] = nameRomaji;
-    _data['image'] = image;
-    return _data;
-  }
+  factory Album.fromJson(Map<String, dynamic> json) => Album(
+        id: json["id"],
+        name: json["name"],
+        nameRomaji: json["nameRomaji"] == null ? null : json["nameRomaji"],
+        image: json["image"] == null ? null : json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "nameRomaji": nameRomaji == null ? null : nameRomaji,
+        "image": image == null ? null : image,
+      };
 }
