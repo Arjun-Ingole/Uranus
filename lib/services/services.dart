8a0b20275adf03faf0b82eb2c2e4b1c0c8b95078
arjun-ingole <arjuningole38@gmail.com>
2022-08-23 23:25:03 +0530

    Services: Write a function to get image url
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 import 'package:uranus/widgets/switchBar.dart';
+import 'package:uranus/models/SocketData.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 late int heartbeat;
+String default_image =
+    'https://rare-gallery.com/thumbnail/394707-wallpaper-error-404-anime-4k-hd.jpg';
 
 String getSocketURL() {
   String url = 'wss://listen.moe/gateway_v2';
@@ -37,4 +40,19 @@ Future<void> sendPings(WebSocketChannel channel, int heartbeat) async {
       connect();
     }
   });
+}
+
+String getImage(MusicData data) {
+  String image = default_image;
+  final List<Album> album_list = data.d.song.albums;
+  try {
+    for (int i = 0; i < album_list.length; i++) {
+      if (album_list[i].image != null) {
+        image = "https://cdn.listen.moe/covers/" + album_list[i].image!;
+      }
+    }
+  } catch (e) {
+    image = default_image;
+  }
+  return image;
 }
